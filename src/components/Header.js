13a71173bc5de4886c9c5ef744984388725f27ee
vishal <vishalraoo9838@gmail.com>
2023-10-16 18:30:24 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+  <>
+    <header>
+        <span>
+            <img src="brand-logo.png" alt="brand logo" />
+        </span>
+        <span>
+            <input type="search" placeholder="Search Courses..."/>
+        </span>
+        <span>
+            <p>Username</p>
+        </span>
+    </header>
+  </>
+  );
+};
+
+export default Header;
